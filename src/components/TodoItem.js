70b5e13e633f03c157a5802dc8e146cd1d32da47
Alexander Santiago Cárdenas Padilla <alexander.cardenas10@outlook.com>
2022-05-30 11:21:56 -0500

    Add TodoItem.js and TodosList.js Components

---
+++ src/components/TodoItem.js
@@ -0,0 +1,16 @@
+/* eslint react/prop-types: 0 */
+import React from 'react';
+
+class TodoItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const { todo } = this.props;
+    return <li>{todo.title}</li>;
+  }
+}
+
+export default TodoItem;
---
+++ src/components/TodosList.js
@@ -0,0 +1,23 @@
+/* eslint react/prop-types: 0 */
+import React from 'react';
+import TodoItem from './TodoItem';
+
+class TodosList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const { todos } = this.props;
+    return (
+      <ul>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default TodosList;
